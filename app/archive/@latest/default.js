8ed60a5c87cc323067a5e5ee7a08cf670b39c0f4
Md Abu Zafar <mdabuzafar78@gmail.com>
2024-06-16 16:07:04 +0530

    build parallel routes

---
+++ app/archive/@latest/default.js
@@ -0,0 +1,15 @@
+import NewsList from '@/components/news-list/news-list.component';
+import { getLatestNews } from '@/lib/news';
+
+const LatestNewsPage = () => {
+  const latestNews = getLatestNews();
+
+  return (
+    <>
+      <h2>Latest News</h2>
+      <NewsList news={latestNews} />
+    </>
+  );
+};
+
+export default LatestNewsPage;
