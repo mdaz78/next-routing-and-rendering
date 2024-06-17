e2a0fcdf64ce594508b954f0dd2ae20bb8733afc
Md Abu Zafar <mdabuzafar78@gmail.com>
2024-06-17 16:39:10 +0530

    create interceptor route

---
+++ app/news/(newsDetails)/[slug]/@modal/(.)image/page.js
@@ -0,0 +1,27 @@
+'use client';
+
+import { DUMMY_NEWS } from '@/data/dummy-news';
+import { notFound, useRouter } from 'next/navigation';
+
+const InterceptedImagePage = ({ params }) => {
+  const router = useRouter();
+  const newsSlug = params?.slug;
+  const newsItem = DUMMY_NEWS.find((news) => news.slug === newsSlug);
+
+  if (!newsItem) {
+    notFound();
+  }
+
+  return (
+    <>
+      <div className='modal-backdrop' onClick={router.back} />
+      <dialog className='modal' open>
+        <div className='fullscreen-image'>
+          <img src={`/images/news/${newsItem.image}`} alt={newsItem.title} />
+        </div>
+      </dialog>
+    </>
+  );
+};
+
+export default InterceptedImagePage;
